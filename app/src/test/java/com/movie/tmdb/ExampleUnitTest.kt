9f7e4f23c18f18b36576a9963a1ce21ffc9a1932
Abdelhamid <abdelhamid.f.nasser@gmail.com>
2025-07-23 20:20:55 +0300

    chore(deps): add testing dependencies

--- app/src/test/java/com/movie/tmdb/ExampleUnitTest.kt
+++ app/src/test/java/com/movie/tmdb/ExampleUnitTest.kt
@@ -1,17 +1,35 @@
 package com.movie.tmdb
 
-import org.junit.Test
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth.assertThat
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@OptIn(ExperimentalTime::class)
 class ExampleUnitTest {
-    @Test
+
+	@BeforeEach
+	fun setup() {
+		MockKAnnotations.init(this)
+	}
+
+
+	@MockK
+	lateinit var time: Instant
+
+	@Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+		assertThat(true).isTrue()
+		assertThat(1+2).isEqualTo(3)
+		assertThat(time).isNotNull()
     }
 }
